76ae96f19c425e7f6bb2463ba98fb448e7b198d4
Douglas Sellers <douglas.sellers@gmail.com>
2010-08-08 21:09:08 -0700

    Made it so that display syntax tree now takes advantage of accumulating the top levels and displays much nicer output

--- scripts/display_syntax_tree.rb
+++ scripts/display_syntax_tree.rb
@@ -1,4 +1,12 @@
 require 'lib/herbgobbler'
 
 file_name = ARGV.first
-puts ErbFile.load( file_name ).to_s
+top_levels = ErbFile.load( file_name ).accumulate_top_levels
+
+puts "---------- Original ----------------"
+puts File.read( file_name )
+puts "---------- Syntax Tree -------------"
+top_levels.each do |top_level|
+  puts "#{top_level.node_name}: #{top_level.text_value}"
+end
+
