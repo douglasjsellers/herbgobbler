dc3c1cbaff24ecedc685f72aaed1af89f19cea91
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-10 22:31:01 -0700

    Bold tags was still busted

--- scripts/save_result.rb
+++ scripts/save_result.rb
@@ -2,3 +2,5 @@ test_to_save = ARGV.first
 test_name = test_to_save.split( '/' ).last
 
 `ruby scripts/extract_text_from_erb.rb #{test_to_save} > tests/integration/result/#{test_name}.result`
+
+puts "Wrote: tests/integration/result/#{test_name}.result"
