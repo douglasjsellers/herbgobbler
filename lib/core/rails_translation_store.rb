4998eb7173d4141fbcd984005546949ae199693e
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-12 21:39:02 -0700

    Made some basic serialization work

--- lib/core/rails_translation_store.rb
+++ lib/core/rails_translation_store.rb
@@ -6,5 +6,20 @@ class RailsTranslationStore < BaseTranslationStore
   end
   
   def serialize
+    to_return = "#{@language}:\n"
+
+    self.each do |context, key, value|
+      to_return << "#{build_whitespace( 2 )}#{context}:\n"
+      to_return << "#{build_whitespace( 4 )}#{key}: \"#{value}\""
+    end
+    
+    to_return
   end
+
+  private
+
+  def build_whitespace( amount )
+    " " * amount
+  end
+  
 end
--- tests/specs/rails_translation_store_spec.rb
+++ tests/specs/rails_translation_store_spec.rb
@@ -11,9 +11,20 @@ describe RailsTranslationStore do
       key.should == "key"
       value.should == "value"
     end
-    
   end
-  
-  
+
+
+  it "should be able to serialize out a simple language, key and value" do
+    store = RailsTranslationStore.new
+    store.start_new_context( "test" )
+    store.add_translation( "key", "value" )
+    resulting_string =<<SIMPLE_KEY_VALUE
+en:
+  test:
+    key: "value"
+SIMPLE_KEY_VALUE
+
+    store.serialize.should == resulting_string.strip
+  end
   
 end
