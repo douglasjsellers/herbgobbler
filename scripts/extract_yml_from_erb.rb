91d436bceb5f6e461d58ba1e39f1d96624f710f3
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-16 23:45:08 -0700

    Started to integrate some of the yml files into the test suite

---
+++ scripts/extract_yml_from_erb.rb
@@ -0,0 +1,11 @@
+require 'lib/herbgobbler'
+
+file_name = ARGV.first
+rails_translation_store = RailsTranslationStore.new
+text_extractor = RailsTextExtractor.new( rails_translation_store )
+rails_translation_store.start_new_context( file_name.split('.').first )
+erb_file = ErbFile.load( file_name )
+erb_file.extract_text( text_extractor )
+
+print rails_translation_store.serialize
+
--- scripts/save_result.rb
+++ scripts/save_result.rb
@@ -1,6 +1,9 @@
 test_to_save = ARGV.first
 test_name = test_to_save.split( '/' ).last
 
-`ruby scripts/extract_text_from_erb.rb #{test_to_save} > tests/integration/result/#{test_name}.result`
+`ruby scripts/extract_text_from_erb.rb #{test_to_save} > tests/integration/result/erb/#{test_name}.result`
 
-puts "Wrote: tests/integration/result/#{test_name}.result"
+`ruby scripts/extract_yml_from_erb.rb #{test_to_save} > tests/integration/result/yml/#{test_name}.yml.result`
+
+puts "Wrote: tests/integration/result/erb/#{test_name}.result"
+puts "Wrote: tests/integration/result/yml/#{test_name}.yml.result"
