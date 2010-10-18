1fe4e61c51bf393e1efa6559d27fc8b01cb893d8
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-17 22:21:13 -0700

    Made it so that it kind of works to gobble a rails file, something is still screwey with the way that the default rails store saves the keys (no heirarchy)

--- scripts/rails_gobble_file.rb
+++ scripts/rails_gobble_file.rb
@@ -1,17 +1,30 @@
 require 'lib/herbgobbler'
 
-file_name = ARGV.first
-rails_translation_store = RailsTranslationStore.new
-text_extractor = RailsTextExtractor.new( rails_translation_store )
-rails_translation_store.start_new_context( file_name.split('.').first )
-erb_file = ErbFile.load( file_name )
-erb_file.extract_text( text_extractor )
+def print_usage
+  puts "ruby scripts/rails_gobble_file.rb <rails_root> <relative_erb_file_path> <locale_file_name>"
+end
 
-puts erb_file.to_s
+if ARGV.length < 3
+  print_usage()
+else
+  rails_root = ARGV[0]
+  erb_file_path = ARGV[1]
+  locale_file_name = ARGV[2]
+  full_erb_file_path = "#{rails_root}/#{erb_file_path}"
+  full_yml_file_path = "#{rails_root}/config/locales/#{locale_file_name}"
+  
+  rails_translation_store = RailsTranslationStore.new
+  text_extractor = RailsTextExtractor.new( rails_translation_store )
+  rails_translation_store.start_new_context( '') #erb_file_path.split('.').first )
+  erb_file = ErbFile.load( full_erb_file_path )
+  erb_file.extract_text( text_extractor )
 
-puts "-------------"
-
-puts rails_translation_store.serialize
+  File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
+  puts "Wrote #{full_erb_file_path}"
+  
+  File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
+  puts "Wrote #{full_yml_file_path}"
+end
 
 
 
