22f50b50d456475b228926fdca1e90e5de08ecea
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-17 23:56:52 -0700

    Refactored one of the slightly confusing parts of the rails gobble file script

--- scripts/rails_gobble_file.rb
+++ scripts/rails_gobble_file.rb
@@ -4,6 +4,13 @@ def print_usage
   puts "ruby scripts/rails_gobble_file.rb <rails_root> <relative_erb_file_path> <locale_file_name>"
 end
 
+# remove the file extension and the app/views/ so that
+# when the context is set that rails will be able to find
+# it through the default . key syntax
+def convert_path_to_key_path( path )
+  path.split('.').first.gsub( "app/views/", '')  
+end
+
 if ARGV.length < 3
   print_usage()
 else
@@ -15,7 +22,7 @@ else
   
   rails_translation_store = RailsTranslationStore.new
   text_extractor = RailsTextExtractor.new( rails_translation_store )
-  rails_translation_store.start_new_context( erb_file_path.split('.').first.gsub( "app/views/", '') )
+  rails_translation_store.start_new_context( convert_path_to_key_path( erb_file_path ) )
   erb_file = ErbFile.load( full_erb_file_path )
   erb_file.extract_text( text_extractor )
 
