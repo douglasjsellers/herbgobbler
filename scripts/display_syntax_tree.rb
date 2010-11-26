6f3b1d92a0123b43da30c52cca79db7e5bcf4623
Douglas Sellers <douglas.sellers@gmail.com>
2010-11-26 08:50:26 -0800

    Made display syntax tree much smarter to help debug nested parsed element issues

--- scripts/display_syntax_tree.rb
+++ scripts/display_syntax_tree.rb
@@ -1,5 +1,16 @@
 require 'lib/herbgobbler'
 
+@indent = 0
+@print_deep = true
+def print_sub_elements( sub_element )
+  @indent += 5
+  sub_element.elements.each do |element|
+    puts " " * @indent + "#{element.text_value}" if element.respond_to?(:node_name)
+    print_sub_elements( element ) if @print_deep
+  end if sub_element.respond_to?( :elements ) && !sub_element.elements.nil?
+  @indent -= 5
+end
+
 file_name = ARGV.first
 top_levels = ErbFile.load( file_name ).accumulate_top_levels
 
@@ -7,6 +18,7 @@ puts "---------- Original ----------------"
 puts File.read( file_name )
 puts "---------- Syntax Tree -------------"
 top_levels.each do |top_level|
-  puts "#{top_level.node_name}: #{top_level.text_value}"
+  puts "Top Level: #{top_level.node_name}: #{top_level.text_value}"
+  print_sub_elements( top_level )
 end
 
