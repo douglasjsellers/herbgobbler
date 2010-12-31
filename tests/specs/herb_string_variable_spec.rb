63ed0779bb473bcf83b43333c4876dbbdb068472
Douglas Sellers <dsellers@blurb.com>
2010-12-31 14:55:51 -0800

    Made it so that the herb string variable spec now passes, I still don't think that it correctly exctracts variables but it at least finds them

--- tests/specs/herb_string_variable_spec.rb
+++ tests/specs/herb_string_variable_spec.rb
@@ -6,8 +6,24 @@ describe HerbStringVariable do
     erb_file = ErbFile.from_string( '<%= "doug is #{"great"}" %>' )
     nodes = erb_file.combine_nodes( erb_file.nodes )
 
-    nodes.size.should == 5
-    nodes[2].is_a?(HerbStringVariable).should == true
+    
+    nodes.size.should == 1
+    nodes.first.is_a?( HerbNodeRetainingTextNode )
+    nodes.first.nodes.size.should == 7
+    nodes.first.nodes[3].is_a?(HerbStringVariable).should == true
+  end
+
+  it "should be able to extract multiple values from a double quoted string" do
+    erb_file = ErbFile.from_string( '<%= "doug is #{"great"} #{"!"}" %>' )
+    nodes = erb_file.combine_nodes( erb_file.nodes )
+
+    
+    nodes.size.should == 1
+    nodes.first.is_a?( HerbNodeRetainingTextNode )
+    nodes.first.nodes.size.should == 9
+    nodes.first.nodes[3].is_a?(HerbStringVariable).should == true
+    nodes.first.nodes[5].is_a?(HerbStringVariable).should == true
+    
   end
   
 end
