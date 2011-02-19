3fbbe3bb0cfa5425570c83c79203e3c37ea5d54d
Douglas Sellers <douglas.sellers@gmail.com>
2011-02-19 12:09:23 -0800

    Found a small bug in the new gobble

--- lib/commandline/gobble_all.rb
+++ lib/commandline/gobble_all.rb
@@ -28,5 +28,10 @@ class GobbleAll
     puts "Wrote #{full_yml_file_path}"
   end
   
-  
+  # remove the file extension and the app/views/ so that
+  # when the context is set that rails will be able to find
+  # it through the default . key syntax
+  def convert_path_to_key_path( path )
+    path.split('.').first.gsub( '/app/views/', '')
+  end
 end
