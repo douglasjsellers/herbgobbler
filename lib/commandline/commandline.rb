e3189389d4fd1c19c2ebf5bb5619b2ed234f563c
Douglas Sellers <douglas.sellers@gmail.com>
2011-02-19 11:56:20 -0800

    Started to refactor the command line processing so that it is possible to just gobble a single file

---
+++ lib/commandline/commandline.rb
@@ -0,0 +1,1 @@
+require 'commandline/gobble_all'
---
+++ lib/commandline/gobble_all.rb
@@ -0,0 +1,32 @@
+class GobbleAll
+
+  def initialize( rails_root )
+    @rails_root = rails_root
+  end
+
+  def execute
+    locale_file_name = "en.yml"
+    
+    rails_view_directory = "#{@rails_root}/app/views"
+    full_yml_file_path = "#{@rails_root}/config/locales/#{locale_file_name}"
+    
+    rails_translation_store = RailsTranslationStore.new
+    text_extractor = RailsTextExtractor.new( rails_translation_store )
+    
+    Dir["#{rails_view_directory}/**/*html.erb" ].each do |full_erb_file_path|
+      
+      erb_file = full_erb_file_path.gsub( @rails_root, '' )
+      rails_translation_store.start_new_context( convert_path_to_key_path( erb_file.to_s ) )
+      erb_file = ErbFile.load( full_erb_file_path )
+      erb_file.extract_text( text_extractor )
+
+      File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
+      puts "Wrote #{full_erb_file_path}"
+    end
+
+    File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
+    puts "Wrote #{full_yml_file_path}"
+  end
+  
+  
+end
