26e07da5d3af91f838554962be569e4e9510dc66
Douglas Sellers <douglas.sellers@gmail.com>
2012-03-26 23:00:02 -0700

    Added in a string for tr8n that handles all of the character substituiton

---
+++ lib/nodes/herb_tr8n_string_node.rb
@@ -0,0 +1,48 @@
+# This class is mostly responsible for all of the escaping that needs
+# to occur for Tr8n to correctly understand the string.
+
+class HerbTr8nStringNode
+
+  ESCAPE_LOOKUP_TABLE = [
+                         ["\n", "break"],
+                         ["&ndash;", "ndash"],
+                         ["&mdash;", "mdash"],
+                         ["&iexcl;", "iexcl"],
+                         ["&iquest;", "iquest"],
+                         ["&quot;", "quot"],
+                         ["&ldquo;", "ldquo"],
+                         ["&rdquo;", "rdquo"],
+                         ["&lsquo;", "lsquo"],
+                         ["&rsquo;", "rsquo"],
+                         ["&laquo;", "laquo"],
+                         ["&raquo;", "raquo"],
+                         ["&nbsp;", "nbsp"]
+                        ]
+
+  def initialize( string )
+    @escaped_string = tr8n_escape( string )
+  end
+
+  def text_value
+    @escaped_string
+  end
+
+  def to_s
+    text_value
+  end
+
+  private
+
+  def replace_character( string, search_character, replace_character )
+    string.gsub( search_character, "{#{replace_character}}" )
+  end
+  
+  def tr8n_escape( string )
+    ESCAPE_LOOKUP_TABLE.each do |lookup_pair|
+      string = replace_character( string, lookup_pair.first, lookup_pair.last )
+    end
+    string
+  end
+  
+  
+end
---
+++ tests/specs/herb_tr8n_string_node_spec.rb
@@ -0,0 +1,118 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe HerbTr8nStringNode do
+  it "should escape new line character" do
+    s = "please escape \n this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {break} this character"
+    
+  end
+
+  it "should escape two \n characters in a row" do
+    s = "please escape \n\n this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {break}{break} this character"
+    
+  end
+
+  it "should escape \n characters book ending a string" do
+    s = "\nplease escape this character\n"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "{break}please escape this character{break}"
+  end
+  
+  
+  it "should escape ndash" do
+    s = "please escape &ndash; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {ndash} this character"
+    
+  end
+
+
+  it "should escape mdash" do
+    s = "please escape &mdash; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {mdash} this character"
+    
+  end
+
+  it "should escape iexcl" do
+    s = "please escape &iexcl; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {iexcl} this character"
+    
+  end
+
+  it "should escape iquest" do
+    s = "please escape &iquest; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {iquest} this character"
+    
+  end
+
+  it "should escape quot" do
+    s = "please escape &quot; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {quot} this character"
+    
+  end
+
+  it "should escape ldquo" do
+    s = "please escape &ldquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {ldquo} this character"
+    
+  end
+
+  it "should escape rdquo" do
+    s = "please escape &rdquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {rdquo} this character"
+    
+  end
+
+  it "should escape lsquo" do
+    s = "please escape &lsquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {lsquo} this character"
+    
+  end
+
+  it "should escape rsquo" do
+    s = "please escape &rsquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {rsquo} this character"
+    
+  end
+
+  it "should escape laquo" do
+    s = "please escape &laquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {laquo} this character"
+    
+  end
+
+  it "should escape raquo" do
+    s = "please escape &raquo; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {raquo} this character"
+    
+  end
+
+  it "should escape nbsp" do
+    s = "please escape &nbsp; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {nbsp} this character"
+    
+  end
+
+  it "should escape all special characters" do
+    s = "please escape \n &ndash; &mdash; &iexcl; &iquest; &quot; &ldquo; &rdquo; &lsquo; &rsquo; &laquo; &raquo; &nbsp; this character"
+    string_node = HerbTr8nStringNode.new( s )
+    string_node.text_value.should == "please escape {break} {ndash} {mdash} {iexcl} {iquest} {quot} {ldquo} {rdquo} {lsquo} {rsquo} {laquo} {raquo} {nbsp} this character"
+    
+  end
+  
+end
+
