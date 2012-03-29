ea745858586314161716e10170fe5a93b51d288c
Douglas Sellers <douglas.sellers@gmail.com>
2012-03-28 23:13:07 -0700

    Yay, it processes blank files correctly

--- scripts/save_result.rb
+++ scripts/save_result.rb
@@ -10,4 +10,5 @@ test_name = test_to_save.split( '/' ).last
 `ruby scripts/extract_yml_from_erb.rb #{test_to_save} > tests/integration/result/yml/#{test_name}.yml.i18n.result`
 
 puts "Wrote: tests/integration/result/erb/#{test_name}.i18n.result"
+puts "Wrote: tests/integration/result/erb/#{test_name}.tr8n.result"
 puts "Wrote: tests/integration/result/yml/#{test_name}.yml.i18n.result"
