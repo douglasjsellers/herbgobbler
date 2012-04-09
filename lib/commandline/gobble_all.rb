ef95aa824099a9b6f0896c5d4468192c0b47a4cd
Douglas Sellers <douglas.sellers@gmail.com>
2012-04-08 22:42:01 -0700

    Updated the gobble files so that they now understand extractor types

--- lib/commandline/gobble_all.rb
+++ lib/commandline/gobble_all.rb
@@ -1,15 +1,50 @@
 class GobbleAll
   include GobbleShare
   
-  def initialize( rails_root, options )
+  def initialize( rails_root, type, options )
     @rails_root = rails_root
     @options = options
+    @text_extractor_type = type
   end
 
   def execute
     puts "Processing all of: #{@rails_root}"
     puts ""
+
+    if( @text_extractor_type == 'tr8n' )
+      execute_tr8n
+    else
+      execute_i18n      
+    end
     
+  end
+  
+  def valid?
+    @options.empty?
+  end
+
+
+  private
+
+  def execute_tr8n
+    rails_view_directory = "#{@rails_root}/app/views"
+    
+    text_extractor = Tr8nTextExtractor.new
+
+    
+    Dir["#{rails_view_directory}/**/*html.erb" ].each do |full_erb_file_path|
+      
+      erb_file = full_erb_file_path.gsub( @rails_root, '' )
+      erb_file = ErbFile.load( full_erb_file_path )
+      erb_file.extract_text( text_extractor )
+
+      File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
+      puts "Wrote #{full_erb_file_path}"
+    end
+
+  end
+  
+  def execute_i18n
     locale_file_name = "en.yml"
     
     rails_view_directory = "#{@rails_root}/app/views"
@@ -30,11 +65,7 @@ class GobbleAll
     end
 
     File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
-    puts "Wrote #{full_yml_file_path}"
-  end
-  
-  def valid?
-    @options.empty?
+    puts "Wrote #{full_yml_file_path}"    
   end
   
 end
--- lib/commandline/gobble_single_file.rb
+++ lib/commandline/gobble_single_file.rb
@@ -1,13 +1,46 @@
 class GobbleSingleFile
   include GobbleShare
   
-  def initialize( rails_root, options )
+  def initialize( rails_root, type, options )
     @rails_root = rails_root
     @options = options
+    @text_extractor_type = type
   end
 
   def execute
     file_name = @options.first
+    if( @text_extractor_type == 'tr8n' )
+      execute_tr8n( file_name )
+    else
+      execute_i18n( file_name )
+    end
+    
+  end
+
+  def valid?
+    @options.size == 1
+  end
+
+  private
+
+  def execute_tr8n( file_name )
+    rails_view_directory = "#{@rails_root}/app/views"
+    erb_file = "/app/#{file_name.gsub( /.*\/app\//, '' )}"
+    puts "Gobbling file: #{file_name}"
+    puts ""
+
+    # load erb file into tr8n translation store
+    text_extractor = Tr8nTextExtractor.new
+    
+    erb_file = ErbFile.load( file_name )
+    erb_file.extract_text( text_extractor )
+    
+    # write file
+    File.open(file_name, 'w') {|f| f.write(erb_file.to_s) }
+    
+  end
+  
+  def execute_i18n( file_name )
     locale_file_name = "en.yml"
     rails_view_directory = "#{@rails_root}/app/views"
     full_yml_file_path = "#{@rails_root}/config/locales/#{locale_file_name}"
@@ -25,16 +58,12 @@ class GobbleSingleFile
     erb_file = ErbFile.load( file_name )
     erb_file.extract_text( text_extractor )
     
-    # wiret file
+    # write file
     File.open(file_name, 'w') {|f| f.write(erb_file.to_s) }
 
     # re-write yml file
-    File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
-
+    File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }    
   end
-
-  def valid?
-    @options.size == 1
-  end
+  
   
 end
